cc3c9b1bf413f1e2240bc3923e4ddf5a75d37b47
P-iknow <apricotsoul@gmail.com>
2019-06-19 21:46:51 +0900

    fix: method 사이 간격 수정

--- Learning-Javascript-Data-Structure-and-Algorithms/LearningJavaScriptDataStructuresandAlgorithmsThirdEdition_Code/src/js/data-structures/set.js
+++ Learning-Javascript-Data-Structure-and-Algorithms/LearningJavaScriptDataStructuresandAlgorithmsThirdEdition_Code/src/js/data-structures/set.js
@@ -2,6 +2,7 @@ export default class Set {
   constructor() {
     this.items = {};
   }
+
   add(element) {
     if (!this.has(element)) {
       this.items[element] = element;
@@ -9,6 +10,7 @@ export default class Set {
     }
     return false;
   }
+
   delete(element) {
     if (this.has(element)) {
       delete this.items[element];
@@ -16,28 +18,34 @@ export default class Set {
     }
     return false;
   }
+
   has(element) {
     return Object.prototype.hasOwnProperty.call(this.items, element);
   }
+
   values() {
     return Object.values(this.items);
   }
+
   union(otherSet) {
     const unionSet = new Set();
     this.values().forEach(value => unionSet.add(value));
     otherSet.values().forEach(value => unionSet.add(value));
     return unionSet;
   }
+
   intersection(otherSet) {
     const intersectionSet = new Set();
     const values = this.values();
     const otherValues = otherSet.values();
     let biggerSet = values;
     let smallerSet = otherValues;
+
     if (otherValues.length - values.length > 0) {
       biggerSet = otherValues;
       smallerSet = values;
     }
+
     smallerSet.forEach(value => {
       if (biggerSet.includes(value)) {
         intersectionSet.add(value);
@@ -45,6 +53,7 @@ export default class Set {
     });
     return intersectionSet;
   }
+
   difference(otherSet) {
     const differenceSet = new Set();
     this.values().forEach(value => {
@@ -54,6 +63,7 @@ export default class Set {
     });
     return differenceSet;
   }
+
   isSubsetOf(otherSet) {
     if (this.size() > otherSet.size()) {
       return false;
@@ -68,15 +78,19 @@ export default class Set {
     });
     return isSubset;
   }
+
   isEmpty() {
     return this.size() === 0;
   }
+
   size() {
     return Object.keys(this.items).length;
   }
+
   clear() {
     this.items = {};
   }
+
   toString() {
     if (this.isEmpty()) {
       return '';
