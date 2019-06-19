50fb436b8cec1a393c02c14a870c41870deff59d
P-iknow <apricotsoul@gmail.com>
2019-06-19 13:01:05 +0900

    add: Learning-Javascript-DataStructure book rep

--- Hello_Coding_Algorithm/ch02_selectionSort/01_selsectionSort.js
+++ Hello_Coding_Algorithm/ch02_selectionSort/01_selsectionSort.js
@@ -1,23 +1,26 @@
 //가장 작은 수를 찾는 함수
 function findSmallest(arr) {
-   var smallest = arr[0], smallestIndex = 0, length = arr.length;
-   for (var i=1; i<length; i++ ) {
-       if(arr[i] < smallest) {
-           smallest = arr[i];
-           smallestIndex = i;
-       }
-   }
-   return smallestIndex; 
+  var smallest = arr[0],
+    smallestIndex = 0,
+    length = arr.length;
+  for (var i = 1; i < length; i++) {
+    if (arr[i] < smallest) {
+      smallest = arr[i];
+      smallestIndex = i;
+    }
+  }
+  return smallestIndex;
 }
 
 function selectionSort(arr) {
-    var smallest, newArr = [], length = arr.length
-    for (var i=0; i<length; i++) {
-        smallest = findSmallest(arr);
-        newArr.push(arr.splice(smallest, 1)[0]);
-    }
-    return newArr;
+  var smallest,
+    newArr = [],
+    length = arr.length;
+  for (var i = 0; i < length; i++) {
+    smallest = findSmallest(arr);
+    newArr.push(arr.splice(smallest, 1)[0]);
+  }
+  return newArr;
 }
 
-
-selectionSort([6,7,3,9,2,1]);
+selectionSort([6, 7, 3, 9, 2, 1]);
